61f5c81cca4e962b6a162df6cac98e9208c86c6b
Nvt5 <121981147+Nvt500@users.noreply.github.com>
2024-11-21 21:31:42 -0500

    Prettier Code. Add LICENSE and README. Can now delete range of tasks.

---
+++ README.md
@@ -0,0 +1,83 @@
+
+# Todo List
+
+Simple todo list cli written in rust.
+
+# Usage
+
+```text
+Simple todo list cli.
+
+Usage: todo.exe <COMMAND>
+
+Commands:
+  show  Show list
+  add   Add task to list
+  del   Remove task from list
+  help  Print this message or the help of the given subcommand(s)
+
+Options:
+  -h, --help     Print help
+  -V, --version  Print version
+```
+
+## Add
+
+```text
+Add task to list
+
+Usage: todo.exe add <TASK>
+
+Arguments:
+  <TASK>  Task to add
+
+Options:
+  -h, --help  Print help
+```
+
+## Delete
+
+```text
+Remove task from list
+
+Usage: todo.exe del <TASK>
+
+Arguments:
+  <TASK>  Task, index, or range (eg. 1-5) to remove
+
+Options:
+  -h, --help  Print help
+```
+
+## Example
+
+```text
+> todo add "First Task"
+Adding task First Task
+> todo add "Second Task"
+Adding task Second Task
+> todo add "Third Task"
+Adding task Third Task
+> todo add "Fourth Task"
+Adding task Fourth Task
+> todo show
+1: First Task
+2: Second Task
+3: Third Task
+4: Fourth Task
+> todo del "First Task"
+Deleting task First Task
+> todo show
+1: Second Task
+2: Third Task
+3: Fourth Task
+> todo del 1
+Deleting task 1
+> todo show
+1: Third Task
+2: Fourth Task
+> todo del 1-2
+Deleting task 1-2
+> todo show
+No tasks.
+```
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use clap::{Parser, Subcommand};
 use std::fs;
 use std::io::{Read, Seek, SeekFrom, Write};
+use std::path::PathBuf;
 
-/// Simple todo list
+
+/// Simple todo list cli
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Args
@@ -25,47 +27,194 @@ enum Commands
 
     /// Remove task from list
     Del {
-        /// Task to remove
+        /// Task, index, or range (eg. 1-5) to remove
         task: String,
     },
 }
 
-fn main() {
+fn get_todo_path() -> Result<PathBuf, &'static str>
+{
+    let todo_path_parent = std::env::current_exe().unwrap_or("".into());
+
+    if todo_path_parent.to_str().unwrap().is_empty()
+    {
+        return Err("Cannot get directory of executable to find todo.txt.");
+    }
+
+    let todo_path = todo_path_parent.parent().unwrap().join(PathBuf::from("todo.txt"));
+
+    Ok(todo_path)
+}
+
+fn main()
+{
     let args = Args::parse();
 
     match args.commands
     {
         Commands::Add { task } => {
-            println!("Adding task {}", task);
-            let mut file = fs::File::options().write(true).read(false).create(true).open("todo.txt").expect("Cannot open file.");
-            file.seek(SeekFrom::End(0)).expect("Cannot seek file.");
-            file.write_all(format!("{}\n", task).as_bytes()).expect("Cannot write to file.");
+            match add(task) {
+                Ok(_) => {},
+                Err(err) => eprintln!("{}", err),
+            }
         },
         Commands::Del { task } => {
-            println!("Deleting task {}", task);
-            let mut file = fs::File::options().write(true).read(true).create(false).open("todo.txt").expect("Cannot open file.");
-            let mut content = String::new();
-            file.read_to_string(&mut content).expect("Cannot read file.");
-            let content = content.lines().filter(|line| line.trim() != task.trim()).collect::<Vec<&str>>();
-            file.set_len(0).ok();
-            file.seek(SeekFrom::Start(0)).ok();
-            file.write_all(format!("{}\n", content.join("\n")).as_bytes()).expect("Cannot write to file.");
+            match del(task) {
+                Ok(_) => {},
+                Err(err) => eprintln!("{}", err),
+            }
         },
         Commands::Show { .. } => {
-            let mut file = fs::File::options().write(true).read(true).create(false).open("todo.txt").expect("Cannot open file.");
-            let mut content = String::new();
-            file.read_to_string(&mut content).expect("Cannot read file.");
-            for (index, line) in content.lines().enumerate()
-            {
-                if line.is_empty() && content.lines().count() == 1
-                {
-                    println!("No tasks.");
-                }
-                else
-                {
-                    println!("{}: {}", index+1, line);
-                }
+            match show() {
+                Ok(_) => {},
+                Err(err) => eprintln!("{}", err),
             }
         }
     }
+}
+
+fn add(task: String) -> Result<(), String>
+{
+    let todo_path = match get_todo_path() {
+        Ok(path) => path,
+        Err(err) => return Err(err.into()),
+    };
+
+    let mut file = match fs::File::options().write(true).read(false).create(true).open(&todo_path) {
+        Ok(f) => f,
+        Err(_) => return Err(format!("Cannot open file: {:#?}.", todo_path)),
+    };
+
+    if file.seek(SeekFrom::End(0)).is_err()
+    {
+        return Err(format!("Cannot seek file: {:#?}.", todo_path));
+    }
+
+    if file.write_all(format!("{}\n", task).as_bytes()).is_err()
+    {
+        return Err(format!("Cannot write to file: {:#?}.", todo_path));
+    }
+
+    println!("Adding task {}", task);
+
+    Ok(())
+}
+
+fn del(task: String) -> Result<(), String>
+{
+    let todo_path = match get_todo_path() {
+        Ok(path) => path,
+        Err(err) => return Err(err.into()),
+    };
+
+    let mut file = match fs::File::options().write(true).read(true).create(false).open(&todo_path) {
+        Ok(f) => f,
+        Err(_) => return Err(format!("Cannot open file: {:#?}.", todo_path)),
+    };
+
+    let mut content = String::new();
+    if file.read_to_string(&mut content).is_err()
+    {
+        return Err(format!("Cannot read file: {:#?}.", todo_path));
+    }
+
+    let mut new_content;
+    if let Ok(index) = task.parse::<usize>()
+    {
+        new_content = content.lines().collect::<Vec<&str>>();
+
+        if index <= 0 || index > new_content.len()
+        {
+            return Err(format!("Index {} is out of range {}-{}", index, 1, new_content.len()));
+        }
+
+        new_content.remove(index - 1);
+
+    }
+    else if let Some(range) = task.split_once("-")
+    {
+        if let Ok(start) = range.0.parse::<usize>()
+        {
+            if let Ok(end) = range.1.parse::<usize>()
+            {
+                new_content = content.lines().collect::<Vec<&str>>();
+                if start > end
+                {
+                    return Err(format!("Start cannot be greater than end, {}-{}.", start, end));
+                }
+                if start <= 0
+                {
+                    return Err("Start is less than 1.".into());
+                }
+                if end > new_content.len()
+                {
+                    return Err(format!("End cannot be greater than max, {}>{}.", end, new_content.len()));
+                }
+
+                for index in (start-1..end).rev()
+                {
+                    new_content.remove(index);
+                }
+            }
+            else
+            {
+                new_content = content.lines().filter(|line| line.trim() != task.trim()).collect::<Vec<&str>>();
+            }
+        }
+        else
+        {
+            new_content = content.lines().filter(|line| line.trim() != task.trim()).collect::<Vec<&str>>();
+        }
+    }
+    else
+    {
+        new_content = content.lines().filter(|line| line.trim() != task.trim()).collect::<Vec<&str>>();
+    }
+    if file.set_len(0).is_err() { return Err(format!("Cannot change file contents: {:#?}.", todo_path)); }
+    if new_content.is_empty()
+    {
+        println!("Deleting task {}", task);
+        return Ok(());
+    }
+    if file.seek(SeekFrom::Start(0)).is_err() { return Err(format!("Cannot change file contents: {:#?}.", todo_path)); }
+
+    if file.write_all(format!("{}\n", new_content.join("\n")).as_bytes()).is_err() { return Err(format!("Cannot write to file: {:#?}.", todo_path)); }
+
+    println!("Deleting task {}", task);
+
+    Ok(())
+}
+
+fn show() -> Result<(), String>
+{
+    let todo_path = match get_todo_path() {
+        Ok(path) => path,
+        Err(err) => return Err(err.into()),
+    };
+
+    let mut file = match fs::File::options().write(true).read(true).create(false).open(&todo_path) {
+        Ok(f) => f,
+        Err(_) => return Err(format!("Cannot open file: {:#?}.", todo_path)),
+    };
+
+    let mut content = String::new();
+
+    if file.read_to_string(&mut content).is_err()
+    {
+        return Err(format!("Cannot read file: {:#?}.", todo_path));
+    }
+
+    if content.trim().is_empty()
+    {
+        println!("No tasks.");
+    }
+    else
+    {
+        for (index, line) in content.lines().enumerate()
+        {
+            println!("{}: {}", index+1, line);
+        }
+    }
+
+    Ok(())
 }
